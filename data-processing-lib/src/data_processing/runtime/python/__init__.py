e19fd03a9ddfe6d6810d0113769fc5dab0eb1ff5
blublinsky <blublinsky@hotmail.com>
2025-04-07 17:07:05 +0100

    Add Ray and Python specific runtime support

---
+++ data-processing-lib/src/data_processing/runtime/python/__init__.py
@@ -0,0 +1,14 @@
+from data_processing.runtime.python.runtime_configuration import (
+    PythonTransformRuntimeConfiguration,
+)
+from data_processing.runtime.python.execution_configuration import (
+    PythonTransformExecutionConfiguration,
+)
+from data_processing.runtime.python.transform_file_processor import (
+    PythonTransformFileProcessor,
+    PythonPoolTransformFileProcessor,
+)
+from data_processing.runtime.python.transform_orchestrator import (
+    PythonTransformOrchestrator,
+)
+from data_processing.runtime.python.transform_launcher import PythonTransformLauncher
